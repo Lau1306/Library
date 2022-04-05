a361f6ef7f77b113f61d5661672ad4841b50aef7
Lau1306 <laura.anlau@gmail.com>
2022-04-04 23:39:08 -0500

    - Agrega escaner para qr

--- screens/transaction.js
+++ screens/transaction.js
@@ -1,11 +1,63 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity  } from "react-native";
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status == "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    });
+  };
+
   render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+
+    if (domState == "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Pantalla de Transacción</Text>
+
+        <Text style={styles.text}>
+          {hasCameraPermissions ? scannedData : "Solicitar permiso de la cámara"}
+        </Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => this.getCameraPermissions("scanner")}>
+          <Text style={styles.buttonText}>Escanear código QR</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -21,5 +73,18 @@ const styles = StyleSheet.create({
   text: {
     color: "#ffff",
     fontSize: 30
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15,
+    marginTop: 25
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF"
   }
 });
