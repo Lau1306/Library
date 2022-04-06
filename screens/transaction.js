7f511c4deeae6a6eb8839c31961a8adb7449b44d
Lau1306 <laura.anlau@gmail.com>
2022-04-05 23:34:32 -0500

    - Actividades clase 70:
    	* Agrega fonts
    	* Agrega imágenes: bg, icon, name
    	* Agrega icons
    	* Agrega form para studentId, bookId

--- screens/transaction.js
+++ screens/transaction.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity  } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, ImageBackground, Image  } from "react-native";
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from "expo-barcode-scanner";
+
+const bgImage = require("../images/background2.png");
+const appIcon = require("../images/appIcon.png");
+const appName = require("../images/appName.png");
 
 export default class TransactionScreen extends Component {
   constructor(props) {
@@ -10,7 +14,9 @@ export default class TransactionScreen extends Component {
       domState: "normal",
       hasCameraPermissions: null,
       scanned: false,
-      scannedData: ""
+      scannedData: "",
+      bookId: "",
+      studentId: "",
     };
   }
 
@@ -28,17 +34,27 @@ export default class TransactionScreen extends Component {
   };
 
   handleBarCodeScanned = async ({ type, data }) => {
-    this.setState({
-      scannedData: data,
-      domState: "normal",
-      scanned: true
-    });
+    const { domState } = this.state;
+
+    if (domState === "bookId") {
+      this.setState({
+        bookId: data,
+        domState: "normal",
+        scanned: true
+      });
+    } else if (domState === "studentId") {
+      this.setState({
+        studentId: data,
+        domState: "normal",
+        scanned: true
+      });
+    }
   };
 
   render() {
-    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    const { domState, bookId, studentId, scanned } = this.state;
 
-    if (domState == "scanner") {
+    if (domState != "normal") {
       return (
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
@@ -49,15 +65,28 @@ export default class TransactionScreen extends Component {
 
     return (
       <View style={styles.container}>
+        <ImageBackground source={bgImage} style={styles.bgImage}>
+          <View style={styles.upperContainer}>
+              <Image source={appIcon} style={styles.appIcon} />
+              <Image source={appName} style={styles.appName} />
+          </View>
 
-        <Text style={styles.text}>
-          {hasCameraPermissions ? scannedData : "Solicitar permiso de la cámara"}
-        </Text>
+          <View style = {styles.lowerContainer}>
+            <View style={styles.textinputContainer}>
+              <TextInput style={styles.textinput} placeholder={"Id del libro"} placeholderTextColor={"#FFFFFF"} value={bookId} />
+              <TouchableOpacity style={styles.scanbutton} onPress={() => this.getCameraPermissions("bookId")} >
+                <Text style={styles.scanbuttonText}>Escanear</Text>
+              </TouchableOpacity>
+            </View>
 
-        <TouchableOpacity style={styles.button} onPress={() => this.getCameraPermissions("scanner")}>
-          <Text style={styles.buttonText}>Escanear código QR</Text>
-        </TouchableOpacity>
-
+            <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+              <TextInput style={styles.textinput} placeholder={"Id del alumno"} placeholderTextColor={"#FFFFFF"} value={studentId} />
+              <TouchableOpacity style={styles.scanbutton} onPress={() => this.getCameraPermissions("studentId")} >
+                <Text style={styles.scanbuttonText}>Escanear</Text>
+              </TouchableOpacity>
+            </View>
+          </View>   
+        </ImageBackground>    
       </View>
     );
   }
@@ -66,25 +95,64 @@ export default class TransactionScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FFFFFF"
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  upperContainer: {
+    flex: 0.5,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#5653D4"
+    alignItems: "center"
   },
-  text: {
-    color: "#ffff",
-    fontSize: 30
+  appIcon: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 80
   },
-  button: {
-    width: "43%",
-    height: 55,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F48D20",
-    borderRadius: 15,
-    marginTop: 25
+  appName: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain"
   },
-  buttonText: {
-    fontSize: 24,
+  lowerContainer: {
+    flex: 0.5,
+    alignItems: "center"
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF"
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
     color: "#FFFFFF"
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold"
   }
 });
